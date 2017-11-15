7852a3dfe9019c5fe7a4f46fbaf75495dc2861a3
Eik-S <eikem@posteo.de>
2017-11-15 23:03:36 +0100

    finished first dict/json draft

--- crawlers/burschenschaftWiki.py
+++ crawlers/burschenschaftWiki.py
@@ -1,7 +1,9 @@
 import urllib.request
+import json
 from bs4 import BeautifulSoup
 
 burschisWikiUrl = "https://de.wikipedia.org/wiki/Liste_der_Burschenschaften"
+baseUrl = "https://de.wikipedia.org"
 req = urllib.request.Request(burschisWikiUrl)
 with urllib.request.urlopen(req) as response:
    html = response.read()
@@ -13,13 +15,72 @@ rows = soup.find_all('tr')
 
 burschis = []
 
+burschiDict = {}
+
 for row in rows:
   if( row.find('span')):
     burschis.append(row)
 
-for burschi in burschis:
-  print( burschi.prettify())
-  data = burschi.find_all('td')
-  for index, line in enumerate(data):
-    print( str(index) + "   " + str(line.find(text=True, recursive=False)))
+for baseIndex, burschi in enumerate(burschis):
+  data = burschi.find_all('td', recursive=False)
 
+  burschiDict[baseIndex] = {}
+
+  for propIndex, line in enumerate(data):
+
+    print( line)
+    lineTexts = []
+    link = ""
+
+    if(line.find('a') != None):
+      lineTexts = line.find('a').find_all(text=True, recursive=False)
+    else:
+      lineTexts = line.find_all(text=True, recursive=False)
+
+    if(line.find('img') != None):
+      link = "https:" + line.find('img')['src']
+
+    mockText = ""  
+    if(len(lineTexts) > 0):
+      mockText = lineTexts[-1]
+    else:
+      mockText = ""
+
+    text = mockText.replace('\n', '')
+    print( "%s:  %s" % (propIndex, text))
+
+    dictPos = burschiDict[baseIndex]
+    if propIndex == 0:
+      dictPos['title'] = text
+    elif propIndex == 1:
+      if('location' in dictPos):
+        dictPos['location']['city'] = text
+      else:
+        dictPos['location'] = {'city': text}
+    elif propIndex == 2:
+      dictPos['year'] = text
+    elif propIndex == 3:
+      dictPos['colors'] = text
+    elif propIndex == 4:
+      dictPos['union'] = text
+    elif propIndex == 5:
+      dictPos['syndicate'] = text
+    elif propIndex == 6:
+      dictPos['incorporation'] = text
+    elif propIndex == 7:
+      if('logo' in dictPos):
+        dictPos['logo']['link'] = link
+      else:
+        dictPos['logo'] = { 'link': link}
+    elif propIndex == 8:
+      if('weirdLogo' in dictPos):
+        dictPos['weirdLogo']['link'] = link
+      else:
+        dictPos['weirdLogo'] = { 'link': link}
+    elif propIndex == 9:
+      dictPos['striking'] = text
+    elif propIndex == 10:
+      dictPos['status'] = text
+
+  print( "\n\n\n")
+print( json.dumps(burschiDict, indent=4, ensure_ascii=False))
