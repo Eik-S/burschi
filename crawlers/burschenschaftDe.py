349e616d5260dec73409d4d4c6431e318d7b4529
Lulu Roth <ls80zyse@studserv.uni-leipzig.de>
2017-11-15 22:57:10 +0100

    refactor functionality

--- crawlers/burschenschaftDe.py
+++ crawlers/burschenschaftDe.py
@@ -1,23 +1,55 @@
 import urllib.request
 from bs4 import BeautifulSoup
 
+
+def log(string, fname):
+    with open('../log/' + fname + '.log', 'a+') as logfile:
+        logfile.write(string + '\n')
+
+
+def get_html(url):
+    req = urllib.request.Request(url)
+    with urllib.request.urlopen(req) as response:
+        return response.read()
+
+
+def get_all_links_with_class_list_group_item(html_doc):
+    result = []
+    soup = BeautifulSoup(html_doc, 'html.parser')
+    for a in soup.find_all('a', attrs={'class': 'list-group-item'}):
+        result.append(a['href'])
+    return result
+
+
+def get_all_links_from_content_div(html_doc):
+    result = []
+    link_soup = BeautifulSoup(html_doc, 'html.parser')
+    for div in link_soup.find_all('div', attrs={'class': 'content'}):
+        for a in div.find_all('a'):
+            result.append(a['href'])
+    return result
+
+
+visited_links = []
+
 baseUrl = 'http://www.burschenschaft.de/'
 domain = 'burschenschaft-in-deutschland-und-oesterreich.html'
 
-req = urllib.request.Request(baseUrl + domain)
-with urllib.request.urlopen(req) as response:
-    html = response.read()
+stadt_page = get_html(baseUrl + domain)
 
-soup = BeautifulSoup(html, 'html.parser')
+staedte_links = get_all_links_with_class_list_group_item(stadt_page)
+visited_link = staedte_links
 
-cityPages = []
 
-for link in soup.find_all('a', attrs={'class': 'list-group-item'}):
-    href = link['href']
-    print(href)
-    req = urllib.request.Request(baseUrl + href)
-    with urllib.request.urlopen(req) as response:
-        cityPages.append(response.read)
+for stadt_link in staedte_links:
+    stadt_page = get_html(baseUrl + stadt_link)
+    for content_link in get_all_links_from_content_div(stadt_page):
+        if content_link in visited_links:
+            continue
+        elif content_link not in visited_links:
+            visited_links.append(content_link)
+            log(content_link, 'content_links')
+
 
 
 
