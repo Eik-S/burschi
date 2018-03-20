9377c0119f5461f1e90330d49aea827dc3ddec46
h1-c <johannes.heinz@posteo.de>
2018-03-20 14:20:03 +0100

    added functionality to weblinks.py and edited main.py to create list of all links

--- crawlers/webLinks.py
+++ crawlers/webLinks.py
@@ -1,16 +1,26 @@
 import urllib.request
 import json
 from bs4 import BeautifulSoup
+from urllib.parse import urlparse
+from urllib.parse import urljoin
 import ssl
+import requests
 
 def get_external_links(input_url):
-
+    
     ctx = ssl.create_default_context()
     ctx.check_hostname = False
     ctx.verify_mode = ssl.CERT_NONE
+    
+    if not input_url.startswith('http://') and not input_url.startswith('https://'):
+        input_url = 'http://' + input_url
+        
+    #cut parent domain from input url:
+    parent_domain = urlparse(input_url).netloc
+    if parent_domain.startswith('www.'):
+        parent_domain = parent_domain[4:]
+    
 
-
- test = 1
     int_links = set()
     int_links_checked = set()
     ext_links = [
@@ -24,38 +34,75 @@ def get_external_links(input_url):
         #    "count": 3
         #}
     ]
+    
+    int_links.add(input_url)
 
     def get_next_html(url):
-        #TODO: Navigate to next subpage and return html_doc
-        int_links.remove(url)
-        req = urllib.request.Request(url)
-        int_links_checked.add(url)
-        with urllib.request.urlopen( req, context=ctx) as response:
-            return response.read
+        req = requests.get(url).text
+        return req
+        #the following 
+        #with urllib.request.urlopen(req, context=ctx) as response:
+         #   return response.read    
 
     def get_links(html_doc):
-        #TODO: get all links and return them as a list
-        return 'foo'
+        result = []
+        soup = BeautifulSoup(html_doc, 'html.parser')
+        for a in soup.find_all('a'):
+            if a.get('href') != None:
+                result.append(a)
+            else:
+                continue
+        for area in soup.find_all('area'):
+            if area.get('href') != None:
+                result.append(area)
+            else:
+                continue
+        return result
 
-
-    int_links.add(input_url)
-    #Main logic:
-    # int_links and int_links_checked are sets 
-    # ext_links is the object being returned at the end
-    # 
     while len(int_links) > 0:
-        # TODO:
-        # 1. call get_links with input url
-        # 2. add ext_links & int_links to their sets
-        # (if ext_links already contains dict with link = some_ext_link, 
-        #   increment count)
-        return "bar"
-    return ext_links
-
-    #issues could be:
-    #
-    # - internal url could be a sub domain
-    #   e.g. http://burschi.de as main url
-    #       http://kontakt.burschi.de as sub url
-    #   so better cut http:// or https:// before checking
-    #   if a domain is internal.
+            try:
+                    url = int_links.pop()
+                    int_links_checked.add(url)
+                    if get_next_html(url) != None:
+                        html_doc = get_next_html(url)
+                        if get_links(html_doc) != None:
+                            link_list = get_links(html_doc)
+                            
+                            #decide if link in output of get_links is internal or external
+                            #append internal links to int_links, for external link
+                            #create new dict in ext_links or increment count if link is 
+                            #already present
+                            for link in link_list:
+                                l = link.get('href')
+                                if l not in int_links_checked:
+                                    if l.startswith('http://') or l.startswith('https://') or l.startswith('www.'):
+                                        if parent_domain not in l:
+                                            check = False
+                                            for dict in ext_links:
+                                                if dict['link'] == l:
+                                                    dict['count'] =+ 1
+                                                    check = True
+                                            if not check:
+                                                ext_links.append({'link': l, 'count': 1})
+                                        elif l.count('http') > 1 or l.count('www') > 1:
+                                            check = False
+                                            for dict in ext_links:
+                                                if dict['link'] == l:
+                                                    dict['count'] =+ 1
+                                                    check = True
+                                            if not check:
+                                                    ext_links.append({'link': l, 'count': 1})
+                                        else:
+                                            int_links.add(l)
+                                    elif not l.startswith('http://') and not l.startswith('https://')  and not l.startswith('www.'):
+                                        int_links.add(urljoin(input_url, l))
+                                else:
+                                    continue
+                            else:
+                                continue
+                    else:
+                        continue
+            except:
+                pass
+            
+            return ext_links
--- main.py
+++ main.py
@@ -2,9 +2,17 @@ import crawlers.burschenschaftDe as burschiDeCrawler
 import crawlers.webLinks as webLinksCrawler
 
 burschis = burschiDeCrawler.get_burschi_info()
+
+links_all_burschis = [] #list of dicts with all external links
+
 for burschi in burschis:
     url = burschi["web_link"]
     page_links = webLinksCrawler.get_external_links(url)
-    burschi["page_links"] = page_links
-
-print( burschis)
+    print(page_links)
+    #add dicts with ext links to list or increment count
+    for dict in page_links:
+        if dict['link'] in links_all_burschis:
+            dict['count'] += 1
+        else:
+            links_all_burschis.append(dict)
+print(links_all_burschis)
