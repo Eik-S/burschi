7767835e5dee60312544695f36d4a1447ab8da70
Eik-S <eikem@posteo.de>
2018-03-21 12:29:07 +0100

    blablabla

--- crawlers/webLinks.py
+++ crawlers/webLinks.py
@@ -5,13 +5,14 @@ from urllib.parse import urlparse
 from urllib.parse import urljoin
 import ssl
 import requests
+import sys
 
 def get_external_links(input_url):
     
     ctx = ssl.create_default_context()
     ctx.check_hostname = False
     ctx.verify_mode = ssl.CERT_NONE
-    
+
     if not input_url.startswith('http://') and not input_url.startswith('https://'):
         input_url = 'http://' + input_url
         
@@ -37,72 +38,67 @@ def get_external_links(input_url):
     
     int_links.add(input_url)
 
+    # returns the html string of an url
     def get_next_html(url):
-        req = requests.get(url).text
-        return req
-        #the following 
-        #with urllib.request.urlopen(req, context=ctx) as response:
-         #   return response.read    
+        try: 
+            return requests.get(url, verify=False).text
+        except:
+            # print("### EXCEPTION WHILE REQUESTING HTML: %s\n%s\n%s" % (sys.exc_info()[0],sys.exc_info()[1], sys.exc_info()[2]))
+            print("ERROR: %s" % url)
+
+    # def get_next_html(url):
+    #     try:
+    #         req = urllib.request.Req
+    #         with urllib.request.urlopen(url) as response:
+    #             return response.read
+    #     except:
+    #         try:
+    #             with urllib.request.urlopen(url, context=ctx) as response:
+    #                 return response.read    
+    #         except:
+    #             print("### EXCEPTION WHILE REQUESTING HTML: %s\n%s\n%s" % (sys.exc_info()[0],sys.exc_info()[1], sys.exc_info()[2]))
+    #     #the following 
 
     def get_links(html_doc):
         result = []
         soup = BeautifulSoup(html_doc, 'html.parser')
         for a in soup.find_all('a'):
-            if a.get('href') != None:
-                result.append(a)
-            else:
-                continue
+            link = a.get('href')
+            if link != None:
+                result.append(link)
         for area in soup.find_all('area'):
-            if area.get('href') != None:
-                result.append(area)
-            else:
-                continue
+            link = area.get('href')
+            if link != None:
+                result.append(link)
         return result
 
     while len(int_links) > 0:
-            try:
-                    url = int_links.pop()
-                    int_links_checked.add(url)
-                    if get_next_html(url) != None:
-                        html_doc = get_next_html(url)
-                        if get_links(html_doc) != None:
-                            link_list = get_links(html_doc)
-                            
-                            #decide if link in output of get_links is internal or external
-                            #append internal links to int_links, for external link
-                            #create new dict in ext_links or increment count if link is 
-                            #already present
-                            for link in link_list:
-                                l = link.get('href')
-                                if l not in int_links_checked:
-                                    if l.startswith('http://') or l.startswith('https://') or l.startswith('www.'):
-                                        if parent_domain not in l:
-                                            check = False
-                                            for dict in ext_links:
-                                                if dict['link'] == l:
-                                                    dict['count'] =+ 1
-                                                    check = True
-                                            if not check:
-                                                ext_links.append({'link': l, 'count': 1})
-                                        elif l.count('http') > 1 or l.count('www') > 1:
-                                            check = False
-                                            for dict in ext_links:
-                                                if dict['link'] == l:
-                                                    dict['count'] =+ 1
-                                                    check = True
-                                            if not check:
-                                                    ext_links.append({'link': l, 'count': 1})
-                                        else:
-                                            int_links.add(l)
-                                    elif not l.startswith('http://') and not l.startswith('https://')  and not l.startswith('www.'):
-                                        int_links.add(urljoin(input_url, l))
-                                else:
-                                    continue
-                            else:
-                                continue
-                    else:
-                        continue
-            except:
-                pass
-            
-            return ext_links
+        url = int_links.pop()
+        if url in int_links_checked:
+            continue
+        else:
+            int_links_checked.add(url)
+        if url.endswith(".jpg") or url.endswith(".png") or url.endswith(".pdf"):
+            continue
+        html_doc = get_next_html(url)
+        if html_doc == None:
+            continue
+        link_list = get_links(html_doc)
+        if link_list == None:
+            continue
+        for l in link_list:
+            if l.startswith('http://') or l.startswith('https://') or l.startswith('www.'):
+                if parent_domain not in l:
+                    check = False
+                    for dict in ext_links:
+                        if dict['link'] == l:
+                            dict['count'] += 1
+                            check = True
+                    if not check:
+                        ext_links.append({'link': l, 'count': 1})
+                else:
+                    int_links.add(l)
+            else:
+                int_link = urljoin(input_url, l);
+                int_links.add(int_link)
+    return ext_links
--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 import crawlers.burschenschaftDe as burschiDeCrawler
 import crawlers.webLinks as webLinksCrawler
+import json
 
 burschis = burschiDeCrawler.get_burschi_info()
+print("%s burschis collected" % len(burschis))
 
 links_all_burschis = [] #list of dicts with all external links
-
+print("Collecting external links for burschis.")
+i = 0
 for burschi in burschis:
+    i += 1
     url = burschi["web_link"]
     page_links = webLinksCrawler.get_external_links(url)
-    print(page_links)
-    #add dicts with ext links to list or increment count
-    for dict in page_links:
-        if dict['link'] in links_all_burschis:
-            dict['count'] += 1
-        else:
-            links_all_burschis.append(dict)
-print(links_all_burschis)
+    print("%s. %s links added for %s" % (i,len(page_links), url))
+    if len(page_links) > 0:
+        #add dicts with ext links to list or increment count
+        name = burschi["name"].replace(" ","")
+        filename = "./results/%s.txt" % name
+        with open(filename, 'w') as out:
+            out.write(json.dumps(page_links))
